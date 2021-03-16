351610c8b9792ec717209804df8e0fe3fb4a6340
Maciej <maciej.skwara2@gmail.com>
2021-03-16 23:30:53 +0100

    states_node-add velocity calculation

--- scripts/states_node.py
+++ scripts/states_node.py
@@ -1,4 +1,5 @@
 import rospy
+import numpy as np
 
 from nav_msgs.msg import Odometry
 # from tf.transformations import euler_from_quaternion, quaternion_from_euler
@@ -12,18 +13,41 @@ class State:
         self.lisiner = rospy.Subscriber("/base_pose_ground_truth", Odometry, self.callback)
         self.states_pub = rospy.Publisher('/prius/states', States, queue_size=1)
 
+        self.x_prev = 0
+        self.y_prev = 0
+        self.time_prev = 0
+
     def callback(self, data):
-        print('X: ',data.pose.pose.position.x, 'Y: ',data.pose.pose.position.y, 'Z: ',data.pose.pose.position.z)
-        print('Z angle: ',data.twist.twist.angular.z)
+        # print(data.header.stamp)
+        # print('X: ',data.pose.pose.position.x, 'Y: ',data.pose.pose.position.y, 'Z: ',data.pose.pose.position.z)
+        # print('Z angle: ',data.twist.twist.angular.z)
+
 
         if data is not None:
-            # calculate velocity
-            #.................
             states_msg = States()
-            states_msg.velocity = 0
-            states_msg.steer = data.twist.twist.angular.z
-            self.states_pub.publish(states_msg)
 
+            # get time in seconds
+            time = rospy.get_time()
+
+            # calculate velocity
+            delta_time = (time - self.time_prev)
+
+            if delta_time > 2:
+                pass
+                v = (np.sqrt((data.pose.pose.position.x - self.x_prev) ** 2 + (
+                            data.pose.pose.position.y - self.y_prev) ** 2)) / delta_time
+
+                # convert to km/h (?)
+                v = v * 3.6
+                print v
+                states_msg.velocity = v
+                states_msg.steer = data.twist.twist.angular.z
+                self.states_pub.publish(states_msg)
+
+
+                self.x_prev = data.pose.pose.position.x
+                self.y_prev = data.pose.pose.position.y
+                self.time_prev = time
 
 if __name__ == '__main__':
     rospy.init_node('states_node')
