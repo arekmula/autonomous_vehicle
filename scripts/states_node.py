7ff60afba7c5e87705014962d0255f1044431ede
Maciej <maciej.skwara2@gmail.com>
2021-03-16 20:54:08 +0100

    states_node-Add lisiner

--- scripts/states_node.py
+++ scripts/states_node.py
@@ -1,3 +1,33 @@
 import rospy
 
-print("test")
+from nav_msgs.msg import Odometry
+
+from av_msgs.msg import States
+
+
+
+def callback(data):
+    print('X: ',data.pose.pose.position.x, 'Y: ',data.pose.pose.position.y, 'Z: ',data.pose.pose.position.z)
+    print('X: ',data.pose.pose.orientation.x, 'Y: ',data.pose.pose.orientation.y, 'Z: ',data.pose.pose.orientation.z)
+    # rospy.loginfo(rospy.get_caller_id() + "Test ", data.data)
+
+
+def lisiner():
+
+    rospy.Subscriber("/base_pose_ground_truth", Odometry, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('states_node')
+    lisiner()
+
+# States.msg
+# Header header
+#
+# # Vehicle velocity [km/h]
+# float64 velocity
+#
+# # Vehicle steering angle
+# float64 steer
+
+
