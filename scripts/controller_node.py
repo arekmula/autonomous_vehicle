10b02aeac50fe230b0bbdbef553c458454c206cf
Arkadiusz Mula <arekmula97@gmail.com>
2021-03-18 09:18:03 +0100

    controller_node - fixed wrong operators

--- scripts/controller_node.py
+++ scripts/controller_node.py
@@ -6,25 +6,26 @@ import time
 from prius_msgs.msg import Control
 from av_msgs.msg import Mode
 
+
 class Keyboard:
-    
+
     def __init__(self):
-        
+
         time.sleep(5)
-        self.control_pub = rospy.Publisher("/prius", Control,queue_size=1)
-        self.mode_pub = rospy.Publisher("/prius/mode", Mode, queue_size =1)
-        
-        self.collectData = False
-        self.selfdrivingState = False
-        
+        self.control_pub = rospy.Publisher("/prius", Control, queue_size=1)
+        self.mode_pub = rospy.Publisher("/prius/mode", Mode, queue_size=1)
+
+        self.collect_data = False
+        self.self_driving_state = False
+
         self.throttle = 0.
         self.brake = 0.
         self.steer = 0.
         self.gear = Control.NO_COMMAND
-        
+
         self.control_msg = Control()
         self.mode_msg = Mode()
-        
+
         keyboard.on_press_key("c", self.key_collect_data, suppress=False)
         keyboard.on_press_key("s", self.key_selfdriving, suppress=False)
         keyboard.on_press_key("0", self.shift_gear, suppress=False)
@@ -35,16 +36,16 @@ class Keyboard:
         keyboard.on_press_key("down", self.brake, suppress=False)
         keyboard.on_press_key("left", self.turn_left, suppress=False)
         keyboard.on_press_key("right", self.turn_right, suppress=False)
-        
-    def key_collect_data(self,e):
-        self.collectData ^= True
+
+    def key_collect_data(self, e):
+        self.collect_data ^= True
         self.publish()
-        
-    def key_selfdriving(self,e):
-        self.selfdrivingState ^= True
+
+    def key_selfdriving(self, e):
+        self.self_driving_state ^= True
         self.publish()
-    
-    def shift_gear(self,e):
+
+    def shift_gear(self, e):
         if e == "0":
             self.gear = Control.NO_COMMAND
         elif e == "1":
@@ -54,55 +55,56 @@ class Keyboard:
         elif e == "3":
             self.gear = Control.REVERSE
         self.publish()
-        
-    def speed_up(self,e):
+
+    def speed_up(self, e):
         self.brake = 0.
-        if self.throttle => 1.:
-            self.throttle =1.
+        if self.throttle >= 1.:
+            self.throttle = 1.
         else:
             self.throttle += 0.1
         self.publish()
-        
-    def brake(self,e):
+
+    def brake(self, e):
         self.throttle = 0.
-        if self.brake => 1.:
+        if self.brake >= 1.:
             self.brake = 1.
         else:
             self.brake += 0.1
         self.publish()
-            
-    def turn_left(self,e):
+
+    def turn_left(self, e):
         if self.steer <= -1.:
-            self.steer =-1.
+            self.steer = -1.
         else:
             self.steer -= 0.1
         self.publish()
 
-    def turn_right(self,e):
-        if self.steer => 1.:
-            self.steer =1.
+    def turn_right(self, e):
+        if self.steer >= 1.:
+            self.steer = 1.
         else:
             self.steer += 0.1
         self.publish()
 
-    def publish(self, event = None):
+    def publish(self, event=None):
         stamp = rospy.Time.now()
-        
+
         # FILL CONTROL
         self.control_msg.header.stamp = stamp
         self.control_msg.brake = self.brake
         self.control_msg.throttle = self.throttle
         self.control_msg.steer = self.steer
-        
+
         # FILL MODE
         self.mode_msg.header.stamp = stamp
-        self.mode_msg.selfdriving = self.selfdrivingState
-        self.mode_msg.collect = self.collectData
-        
+        self.mode_msg.selfdriving = self.self_driving_state
+        self.mode_msg.collect = self.collect_data
+
         # PUBLISH
         self.control_pub.publish(self.control_msg)
         self.mode_pub.publish(self.mode_msg)
-        
+
+
 if __name__ == '__main__':
     rospy.init_node('controler_node')
     keyboard = Keyboard()
