8e8c5a57fc5613667d6568027eef7ef036aa7919
Maciej <maciej.skwara2@gmail.com>
2021-03-26 22:43:05 +0100

    control_prediction-read predicted values from model

--- av_03/scripts/control_prediction.py
+++ av_03/scripts/control_prediction.py
@@ -4,33 +4,102 @@
 import rospy
 import rospkg
 
+from prius_msgs.msg import Control
+from av_msgs.msg import Mode
+from sensor_msgs.msg import Image
+
 # Python imports
 import tensorflow as tf
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+
 
 
 class Predictor:
-    VEHICLE_MAX_SPEED = 130  # Maximum speed of a car in km/h
+    VEHICLE_MAX_SPEED = 130  # Maximum speed of a car in km
 
     rospack = rospkg.RosPack()
     PATH_TO_ROS_PACKAGE = rospack.get_path("av_03")  # Get path of current ros package
 
-    def __init__(self, model_path=None):
 
+    def __init__(self, model_path=None):
+        self.session = tf.compat.v1.keras.backend.get_session()
+
+        self.mode_sub = rospy.Subscriber("/pruis/mode", Mode, self.mode_callback)
+        self.front_img_sub = rospy.Subscriber("/prius/front_camera/image_raw", Image, self.front_camera_callback, queue_size=1)
+
+        self.control_prediction_pub = rospy.Publisher("/prius", Control, queue_size=1)
+
+        self.cv_bridge = CvBridge()
+        self.model_output = 0.0
+        self.angle_prediction = 0.0
+        self.velocity_prediction = 0.0
+        self.test = 0.0
+        # self.last_published_time = rospy.get_rostime()
+        # self.last_published = None
+        # self.timer = rospy.Timer(rospy.Duration(1. / 0.1), self.timer_callback)
+
+        self.model = None
         self.model_path = self.PATH_TO_ROS_PACKAGE + "/cnn_models/model.h5"
         if model_path is not None:
             self.model_path = model_path
 
         # Load model
         try:
-            model = tf.keras.models.load_model(self.model_path)
+            self.model = tf.keras.models.load_model(self.model_path)
             # Print model summary
-            print(model.summary())
+            print(self.model.summary())
         except IOError as e:
             # Handle the exception if model wasn't read correctly
             print(e)
+    #
+    # def timer_callback(self, event):
+    #     if self.last_published and self.last_published_time < rospy.get_rostime() + rospy.Duration(1.0 / 0.1):
+    #         self.front_camera_callback(self.last_published)
+
+    def mode_callback(self, data):
+
+        stamp = rospy.Time.now()
+
+        control_msg = Control()
+
+        control_msg.header.stamp = stamp
+        control_msg.brake = 0.0
+        control_msg.throttle = 0.9
+        control_msg.steer = 0.0
+        control_msg.shift_gears = Control.NO_COMMAND
+
+        self.control_prediction_pub.publish(control_msg)
+
+
+    def front_camera_callback(self, data):
+
+        if data is not None:
+            try:
+                cv_image = self.cv_bridge.imgmsg_to_cv2(data, "bgr8")
+            except CvBridgeError as e:
+                print(e)
+
+        print(cv_image.shape)
+        img = cv2.resize(cv_image, (200, 200)).astype(np.float32)/255
+        print(img.shape)
+        # print(img)
+
+        with self.session.graph.as_default():
+            tf.compat.v1.keras.backend.set_session(self.session)
+            self.model_output = self.model.predict_on_batch(img[np.newaxis, :])
+
+            self.angle_prediction = self.model_output[0][0]
+            self.velocity_prediction = self.model_output[0][1]
+
+            print(self.angle_prediction, self.velocity_prediction)
+
 
 
 if __name__ == '__main__':
     rospy.init_node("control_prediction")
     predictor = Predictor()
     rospy.spin()
+
+# rostopic pub --once /pruis/mode av_msgs/Mode "{header:{seq: 0, stamp:{secs: 0, nsecs: 0}, frame_id: ''}, selfdriving: true, collect: false}"
