9d8a2ce70f5ab894c0bc2eaed46a21e7f45bdd7b
Jakub-Bielawski <bialyqba@gmail.com>
2021-03-28 12:10:42 +0200

    train_av.py : Add image input shape for CNN, delate not neccesary class ( Evaluator) and create_model method od ModelTrainer class, rename fucntions

--- train/train_av.py
+++ train/train_av.py
@@ -10,9 +10,7 @@ import seaborn as sns
 
 # ML, statistics
 import scipy
-from sklearn.model_selection import train_test_split
 from sklearn import metrics
-from sklearn.metrics import confusion_matrix, roc_curve, auc, roc_auc_score
 
 # Tensorflow
 import tensorflow as tf
@@ -27,7 +25,7 @@ from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCh
 from tensorflow.keras.optimizers import Adam
 
 
-def print_trainnig_stats(args):
+def print_trainnig_info(args):
     print("Running with: ")
     print(f"TF version: {tf.__version__}")
     print(f"Keras version: {keras.__version__}")
@@ -44,13 +42,14 @@ class Generators:
     Train, validation and test generators
     """
 
-    def __init__(self, train_data_path, test_data_path, train_df, test_df, batch_size, color_mode, val_split):
+    def __init__(self, train_data_path, test_data_path, train_df, test_df, input_shape, batch_size, color_mode, val_split):
         self.batch_size = batch_size
         self.color_mode = color_mode
+        self.input_shape = input_shape
         if color_mode == 'grayscale':
-            self.img_shape = (200, 200, 1)
+            self.img_shape = (self.input_shape, self.input_shape, 1)
         else:
-            self.img_shape = (200, 200, 3)
+            self.img_shape = (self.input_shape, self.input_shape, 3)
 
         # Base train/validation generator
         _datagen = ImageDataGenerator(
@@ -137,62 +136,6 @@ class ModelTrainer:
 
         return model
 
-    def create_model(self,
-                     kernel_size=(3, 3),
-                     pool_size=(2, 2),
-                     first_filters=32,
-                     second_filters=64,
-                     third_filters=128,
-                     first_dense=256,
-                     second_dense=128,
-                     dropout_conv=0.3,
-                     dropout_dense=0.3):
-        """
-        NOT IMPLEMENTET WELL
-        """
-
-        model = Sequential()
-        # First conv filters
-        model.add(Conv2D(first_filters, kernel_size, activation='relu', padding="same",
-                         input_shape=(self.img_width, self.img_height, 3)))
-        model.add(Conv2D(first_filters, kernel_size, padding="same", activation='relu'))
-        model.add(Conv2D(first_filters, kernel_size, padding="same", activation='relu'))
-        model.add(MaxPooling2D(pool_size=pool_size))
-        model.add(Dropout(dropout_conv))
-
-        # Second conv filter
-        model.add(Conv2D(second_filters, kernel_size, padding="same", activation='relu'))
-        model.add(Conv2D(second_filters, kernel_size, padding="same", activation='relu'))
-        model.add(Conv2D(second_filters, kernel_size, padding="same", activation='relu'))
-        model.add(MaxPooling2D(pool_size=pool_size))
-        model.add(Dropout(dropout_conv))
-
-        # Third conv filter
-        model.add(Conv2D(third_filters, kernel_size, padding="same", activation='relu'))
-        model.add(Conv2D(third_filters, kernel_size, padding="same", activation='relu'))
-        model.add(Conv2D(third_filters, kernel_size, padding="same", activation='relu'))
-        model.add(MaxPooling2D(pool_size=pool_size))
-        model.add(Dropout(dropout_conv))
-
-        model.add(Flatten())
-
-        # First dense
-        model.add(Dense(first_dense, activation="relu"))
-        model.add(Dropout(dropout_dense))
-        # Second dense
-        model.add(Dense(second_dense, activation="relu"))
-        model.add(Dropout(dropout_dense))
-
-        # Out layer
-        model.add(Dense(2, activation="softmax"))
-
-        loss = losses.MeanAbsoluteError()
-        optimizer = Adam(lr=0.0001)
-        metrics_to_monitor = [metrics.MeanAbsoluteError(), metrics.MeanSquaredError()]
-        model.compile(optimizer=optimizer, loss=loss, metrics=metrics_to_monitor)
-
-        return model
-
     def train(self, model, epochs: int, verb: int):
         """
         Train the model
@@ -226,126 +169,41 @@ class ModelTrainer:
         return training
 
 
-class Evaluator:
-    """
-    Evaluaion :predict on test data (not submission data from test folder)
-    and print reports, plot results etc.
-    """
-
-    def __init__(self, model, training, generator, y_true):
-        self.training = training
-        self.generator = generator
-        # predict the data
-        steps = 5
-        self.y_pred_raw = model.predict_generator(self.generator, steps=steps)
-        self.y_pred = np.argmax(self.y_pred_raw, axis=1)
-        self.y_true = y_true[:len(self.y_pred)]
-
-    """
-    Accuracy, evaluation
-    """
-
-    def plot_history(self):
-        """
-        Plot training history
-        """
-        ## Trained model analysis and evaluation
-        f, ax = plt.subplots(1, 2, figsize=(12, 3))
-        ax[0].plot(self.training.history['loss'], label="Loss")
-        ax[0].plot(self.training.history['val_loss'], label="Validation loss")
-        ax[0].set_title('Loss')
-        ax[0].set_xlabel('Epoch')
-        ax[0].set_ylabel('Loss')
-        ax[0].legend()
-
-        # Accuracy
-        ax[1].plot(self.training.history['acc'], label="Accuracy")
-        ax[1].plot(self.training.history['val_acc'], label="Validation accuracy")
-        ax[1].set_title('Accuracy')
-        ax[1].set_xlabel('Epoch')
-        ax[1].set_ylabel('Accuracy')
-        ax[1].legend()
-        plt.tight_layout()
-        plt.show()
-
-    def plot_roc(self):
-        # y_pred_keras = model.predict_generator(test_gen, steps=len(df_val), verbose=1)
-        # Calculate roc
-        fpr_keras, tpr_keras, thresholds_keras = roc_curve(self.y_true, self.y_pred)
-        auc_keras = auc(fpr_keras, tpr_keras)
-
-        plt.figure(1)
-        plt.plot([0, 1], [0, 1], 'k--')
-        plt.plot(fpr_keras, tpr_keras, label='area = {:.3f}'.format(auc_keras))
-        plt.xlabel('False positive rate')
-        plt.ylabel('True positive rate')
-        plt.title('ROC curve')
-        plt.legend(loc='best')
-        plt.show()
-
-    def print_report(self):
-        """
-        Predict and evaluate using ground truth from labels
-        Test generator did not shuffle 
-        and we can use true labels for comparison
-        """
-        # Print classification report
-        print(metrics.classification_report(self.y_true, self.y_pred))
-
-
 def preapare_labels(train_labels_path):
-    first_labels_path = Path(train_labels_path)
-    second_labels_path = Path(train_labels_path)
-
-    first_labels = pd.read_csv(first_labels_path)
-    second_labels = pd.read_csv(second_labels_path)
-
-    first_labels_useful = first_labels[['img_name', 'steer', 'velocity']]
-    second_labels_useful = second_labels[['img_name', 'steer', 'velocity']]
-    # TODO merge datasets
-    # normalize speed
     MAX_VELOCITY = 130.0  # [Km]
-    first_labels_useful['velocity_normalized'] = first_labels_useful.loc[:, 'velocity'] / MAX_VELOCITY
-    second_labels_useful['velocity_normalized'] = second_labels_useful.loc[:, 'velocity'] / MAX_VELOCITY
+    labels_path = Path(train_labels_path)
+    labels = pd.read_csv(labels_path)[['img_name', 'steer', 'velocity']]
+    # normalize speed
+    labels['velocity_normalized'] = labels.loc[:, 'velocity'] / MAX_VELOCITY
 
-    return first_labels_useful
+    return labels
 
 
 def main(args):
     labels = preapare_labels(args.train_labels_path)
-    # Create generators        
+    print(labels)
+
+    # Create generators
     generators = Generators(train_data_path=args.train_data_path,
                             test_data_path=None,
                             train_df=labels,
                             test_df=None,
+                            input_shape=args.input_shape,
                             batch_size=args.batch_size,
                             color_mode=args.input_color_mode,
                             val_split=args.val_split)
     print("\nGenerators created !!")
 
-    print_trainnig_stats(args)
-
+    print_trainnig_info(args)
     input("Press any key to continue...")
+
     # Create and train the model
     trainer = ModelTrainer(generators)
     model = trainer.create_model_from_article()
     model.summary()
-    # print(type(model))
     training = trainer.train(model=model,
                              epochs=args.epochs,
                              verb=args.verbose)
-
-    # Create evaluator instance
-    # evaluator = Evaluator(model, training, generators.test_generator, test_df.label.values)
-
-    # Draw accuracy and loss charts
-    # evaluator.plot_history()
-
-    # ROC curve
-    # evaluator.plot_roc()
-
-    # Classification report
-    # evaluator.print_report()
 
 
 if __name__ == "__main__":
@@ -353,10 +211,11 @@ if __name__ == "__main__":
     parser.add_argument('--train_data_path', type=str, help='Path to train images', required=True)
     parser.add_argument('--train_labels_path', type=str, help='Path to train labels', required=True)
     parser.add_argument('--test_data_path', type=str, help='Path to test images')
+    parser.add_argument('--input_shape', type=int, help='Shape of input image for CNN', default=200)
     parser.add_argument('--val_split', type=float, help='Split between train and validation dataset', default=0.2)
     parser.add_argument('--epochs', type=int, help='Number of epochs of net training', default=2)
     parser.add_argument('--batch_size', type=int, help='Input batch size to network', default=128)
-    parser.add_argument('--input_color_mode', type=str, help='rgb or grayscale input to network', default='grayscale')
+    parser.add_argument('--input_color_mode', type=str, help='rgb or grayscale input to network', default='rgb')
     parser.add_argument('--verbose', type=int, help='Verbosity of code', default=1)
     args, _ = parser.parse_known_args()
     main(args)
